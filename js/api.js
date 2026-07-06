0cd156ef4077ed42a5a010d42c75940d70109779
lanyanic <xiaoyan20267@qq.com>
2026-07-06 16:53:23 +0800

    fix: API_BASE -> ewsz.langaj.cc

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 // EWS - API 客户端
 const API_BASE = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
   ? 'http://localhost:8787'
-  : 'https://ews.langaj.cc';
+  : 'https://ewsz.langaj.cc';
 
 const API = {
   async login(password, username) { return _post('/api/auth/login', { password, username }); },
